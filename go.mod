950c9979293a8319309e78b50b9e03e8f56f7f2d
bkiran6398 <bkiran6398@gmail.com>
2023-05-14 16:51:37 +0530

    mvp

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bkiran6398/firebase_login_with_google
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"log"
+
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	fmt.Println("Server listening on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
